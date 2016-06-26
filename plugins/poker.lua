b3a49a097b01a226b93a8f449c9758f25ebf4229
Godfather021 <MYWAY_AUST@YAHOO.COM>
2016-06-27 01:40:46 +0430

    Mehran HPR Added more futures

--- plugins/poker.lua
+++ plugins/poker.lua
@@ -1,12 +1,60 @@
-local function run(msg, matches)
-if not is_sudo_id(msg.from.id) then
-local text = '😐'
-reply_msg(msg.id, text, ok_cb, false)
-end
-end
-return {
-patterns = {
-    "^😐$"
-},
-run = run
+do
+
+local function pre_process(msg)
+local hpbase = {
+"😐",
+"😐😐",
+"😐😐😐",
+"😐😐😐😐",
+"😐😐😐😐😐",
+"😐😐😐😐😐😐",
+"😐😐😐😐😐😐😐",
+"😐😐😐😐😐😐😐😐",
+"😐😐😐😐😐😐😐😐😐",
+"😐😐😐😐😐😐😐😐😐😐",
+"😐😐😐😐😐😐😐😐😐😐😐",
+"😐😐😐😐😐😐😐😐😐😐😐😐",
+"😐😐😐😐😐😐😐😐😐😐😐😐😐",
 }
+local hpr = hpbase[math.random(#hpbase)]
+
+
+    local hash = 'poker:'..msg.to.id
+    if db:get(hash) and msg.text == "😐" and not is_sudo_id(msg.from.id) then
+         reply_msg(msg.id, hpr, ok_cb, false)
+            return ""
+        end
+    
+        return msg
+    end
+
+  
+--Plug by Mehran_hpr @iGodFather
+
+local function run(msg, matches)
+    chat_id =  msg.to.id
+    
+    if is_sudo_id(msg.from.id) and matches[1] == 'poker' then
+      
+            
+                    local hash = 'poker:'..msg.to.id
+                    db:set(hash, true)
+                    return "turned on"
+  elseif is_sudo_id(msg.from.id) and matches[1] == 'unpoker' then
+                    local hash = 'poker:'..msg.to.id
+                    db:del(hash)
+                    return "turned off"
+end
+
+end
+
+return {
+    patterns = {
+      '^(😐)$',   
+      '^[/!#](poker)$',
+       '^[/!#](unpoker)$'
+    },
+    run = run,
+    pre_process = pre_process
+}
+end
